4c2a3a454f8176d611371009177b310ad904efdb
rokkolesa <rok.kolesa@gmail.com>
2014-11-13 12:48:13 +0100

    Downwards accumulation
    paths (deklaracija dveh infiksnih operacij ++>, <++ plus funkcija,
    plen), down, depths

--- Trees/Trees.hs
+++ Trees/Trees.hs
@@ -14,6 +14,7 @@ data Btree x
 	= Lf x 
 	-- | Branch Br consisting of two children and a label x.
 	| Br (Btree x) x (Btree x)
+		deriving Show
 	
 -- | The fork (f, g) of two functions f and g takes a single value and returns a pair; thus fork (f, g) a = (f a, g a)
 fork :: (a -> b, a -> d) -> a -> (b, d)
@@ -60,3 +61,50 @@ up f = bmap f . subtrees
 -- | Replaces every element with the number of descendants it has.
 ndesc :: Num b => Btree a -> Btree b
 ndesc = up size
+
+infixr :++>:
+
+infixr :<++:
+
+-- | Path constructor.
+data Path x 
+ -- | Single element path.
+ = Sep x 
+ -- | Path of two elements joined with right turn.
+ | Path x :++>: Path x
+ -- | Path of two elements joined with left turn.
+ | Path x :<++: Path x
+	deriving(Show)
+ 
+-- | Wrap takes x to Sep x
+wrapp :: a -> Path a
+wrapp a = Sep a
+
+-- | Map on path.
+mapp :: (a -> b) -> Path a -> Path b
+mapp f (Sep x) = wrapp (f x)
+mapp f (x :<++: y) = (mapp f x) :<++: (mapp f y)
+mapp f (x :++>: y) = (mapp f x) :++>: (mapp f y)
+
+-- | Paths replaces each element of a tree with that element's ancestors.
+paths :: Btree a -> Btree (Path a)
+paths (Lf x) = Lf (wrapp x)
+paths(Br l x r) = Br (bmap ((wrapp x) :<++:) (paths l)) (wrapp x) (bmap ((wrapp x) :++>:) (paths r))
+
+-- | Downwards accumulation. Applies f on every path from paths.
+down :: (Path a -> b) -> Btree a -> Btree b
+down f = bmap f . paths
+
+-- | Path length
+plen :: Num b => Path a -> b
+plen (Sep a) = 1
+plen (a :<++: b) = plen a + plen b
+plen (a :++>: b) = plen a + plen b
+
+-- | Replaces every element with the length of its path of ancestors. 
+depths :: Btree a -> Btree Integer
+depths = down plen
+
+
+-- test variables
+t = Br (Lf "b") "a" (Br (Lf "d") "c" (Lf "e"))
