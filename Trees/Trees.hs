32fa867fad56adbdb884e24298a84441df504ec0
rokkolesa <rok.kolesa@gmail.com>
2014-12-17 15:20:05 +0100

    Dodan izris dreves
    Spremenjena tudi nekatera imena funkcij zaradi konflikta s tistimi v
    dodanih modulih.

--- Trees/Trees.hs
+++ Trees/Trees.hs
@@ -8,6 +8,13 @@ This module concentrates on deriving tidy drawings of trees.
 -}
 module Trees.Trees where
 
+import Diagrams.Prelude
+import Diagrams.Backend.SVG
+import Data.Tree
+import Diagrams.TwoD.Layout.Tree
+
+-- PART I: Computation
+
 -- | Btree stands for Binary Tree.
 data Btree x 
 	-- | Leaf Lf labelled with a single element x.
@@ -15,6 +22,14 @@ data Btree x
 	-- | Branch Br consisting of two children and a label x.
 	| Br (Btree x) x (Btree x)
 		deriving Show
+
+newtype LabelS = LabelS String
+instance Show LabelS where
+	show (LabelS a) = a
+
+newtype LabelC = LabelC Char
+instance Show LabelC where
+	show (LabelC a) = a:[]
 
 -- | The fork (f, g) of two functions f and g takes a single value and returns a pair; thus fork (f, g) a = (f a, g a)
 fork :: (a -> b, a -> d) -> a -> (b, d)
@@ -45,14 +60,14 @@ root (Lf x) = x
 root (Br l x r) = x
 
 -- | Returns number of elements in the given Btree.
-size :: Num b => Btree a -> b
-size (Lf x) = 1
-size (Br l x r) = size l + 1 + size r
+bSize :: Num b => Btree a -> b
+bSize (Lf x) = 1
+bSize (Br l x r) = bSize l + 1 + bSize r
 
 -- | Returns depth of the given Btree.
 depth :: (Num b, Ord b) => Btree a -> b
 depth (Lf x) = 1
-depth (Br l x r) = 1 + max (size l) (size r)
+depth (Br l x r) = 1 + max (bSize l) (bSize r)
 
 -- | Reverses the given Btree.
 brev :: Btree a -> Btree a
@@ -70,43 +85,43 @@ up f = bmap f . subtrees
 
 -- | Replaces every element with the number of descendants it has.
 ndesc :: Num b => Btree a -> Btree b
-ndesc = up size
+ndesc = up bSize
 
 infixr :++>:
 
 infixr :<++:
 
--- | Path constructor.
-data Path x 
- -- | Single element path.
+-- | TPath constructor.
+data TPath x 
+ -- | Single element TPath.
  = Sep x 
- -- | Path of two elements joined with right turn.
- | Path x :++>: Path x
- -- | Path of two elements joined with left turn.
- | Path x :<++: Path x
+ -- | TPath of two elements joined with right turn.
+ | TPath x :++>: TPath x
+ -- | TPath of two elements joined with left turn.
+ | TPath x :<++: TPath x
 	deriving(Show)
  
 -- | Wrap takes x to Sep x
-wrapp :: a -> Path a
+wrapp :: a -> TPath a
 wrapp a = Sep a
 
--- | Map on path.
-mapp :: (a -> b) -> Path a -> Path b
+-- | Map on TPath.
+mapp :: (a -> b) -> TPath a -> TPath b
 mapp f (Sep x) = wrapp (f x)
 mapp f (x :<++: y) = (mapp f x) :<++: (mapp f y)
 mapp f (x :++>: y) = (mapp f x) :++>: (mapp f y)
 
 -- | Paths replaces each element of a tree with that element's ancestors.
-paths :: Btree a -> Btree (Path a)
+paths :: Btree a -> Btree (TPath a)
 paths (Lf x) = Lf (wrapp x)
 paths(Br l x r) = Br (bmap ((wrapp x) :<++:) (paths l)) (wrapp x) (bmap ((wrapp x) :++>:) (paths r))
 
--- | Downwards accumulation. Applies f on every path from paths.
-down :: (Path a -> b) -> Btree a -> Btree b
+-- | Downwards accumulation. Applies f on every TPath from paths.
+down :: (TPath a -> b) -> Btree a -> Btree b
 down f = bmap f . paths
 
--- | Path length
-plen :: Num b => Path a -> b
+-- | TPath length
+plen :: Num b => TPath a -> b
 plen (Sep a) = 1
 plen (a :<++: b) = plen a + plen b
 plen (a :++>: b) = plen a + plen b
@@ -129,33 +144,66 @@ lzip f (x, []) = x
 lzip f (x:xs, y:ys) = [f x y] ++ lzip f (xs, ys)
 
 -- | Takes a binary tree and returns a list of lists, each containing elements that belong to the same level.
-levels :: Btree a -> [[a]]
-levels (Lf x) = (wrapl.wrapl) x
-levels (Br l x r) = (wrapl.wrapl) x ++ (lzip (++) (levels l, levels r))
+tLevels :: Btree a -> [[a]]
+tLevels (Lf x) = (wrapl.wrapl) x
+tLevels (Br l x r) = (wrapl.wrapl) x ++ (lzip (++) (tLevels l, tLevels r))
 
 -- | Infix function that calculates the width of the narrowest part of the gap between the trees.
 (<+>) :: (Num a, Ord a) => Btree a -> Btree a -> a
-p <+> q = smallest(szip (\x -> \y -> y - x) (map largest (levels p), map smallest (levels q)))
+p <+> q = smallest(szip (\x -> \y -> y - x) (map largest (tLevels p), map smallest (tLevels q)))
 
 -- | Replaces every element with x coordinates relative to root node.
-bdraw :: (Ord b, Fractional b) => Btree a -> Btree b
+bdraw :: Btree a -> Btree Double
 bdraw (Lf a) = Lf 0
-bdraw (Br l x r) = Br (bmap (+(negate d)) (bdraw l)) 0 (bmap (+d) (bdraw r)) where d = sep(Br l x r)
+bdraw (Br l x r) = Br (bmap (+(negate d)) (bdraw l)) 0 (bmap (+d) (bdraw r)) where d = tSep(Br l x r)
 
 -- | Calculates optimal offset for children nodes for the given tree.
-sep (Lf a) = 0
-sep (Br l x r) = (1 - (bdraw l <+> bdraw r)) / 2
+tSep (Lf a) = 0
+tSep (Br l x r) = (1 - (bdraw l <+> bdraw r)) / 2
 
 -- | Gives the relative position of every parent.
---rel :: Btree a -> Btree Double
---rel = bmap sep.subtrees
-rel :: (Ord b, Fractional b) => Btree a -> Btree b
+rel :: Btree a -> Btree Double
 rel (Lf _) = Lf 0
-rel (Br l x r) = Br (rel l)  (sep (Br l x r)) (rel r)
+rel (Br l x r) = Br (rel l)  (tSep (Br l x r)) (rel r)
 
 tabs (Lf _) = Lf 0
 tabs (Br l x r) = Br (bmap (+(negate x)) (tabs l)) 0 (bmap (+x) (tabs r))
 
+
+-- PART II: Drawing
+drawSVG :: Show a => FilePath -> Btree a -> IO ()
+drawSVG fname t = renderSVG fname (mkSizeSpec (Just 1000.0) (Just 1000.0)) (tRender t)
+
+tRender t = renderTree (\n -> (alignedText 0.5 0.7 (show n) # fontSizeL 0.3 <> circle 0.3 # fc white)) (~~) (mkTree t (addLevels (bdraw t) 0)) # centerXY # pad 1.1
+
+mkTree :: Btree a -> Btree (Double, Double) -> Tree (a, P2)
+mkTree (Lf el) (Lf (x,y)) = Node(el, p2(x,y)) []
+mkTree (Br l el r) (Br l' (x,y) r') = Node(el, p2(x,y)) [mkTree l l', mkTree r r']
+mkTree _ _= error "Trees are not the same size!"
+
+addLevels :: Btree a -> Double -> Btree (a, Double)
+addLevels (Lf x) i = Lf (x, i)
+addLevels (Br l x r) i = Br (addLevels l (i-1)) (x, i) (addLevels r (i-1)) 
+
 -- test variables
-t = Br (Lf "b") "a" (Br (Lf "d") "c" (Lf "e"))
-v = Br (Br (Lf "f") "b" (Lf "g")) "a" (Br (Lf "d") "c" (Lf "e"))
+t1 = Br (Lf (LabelS "b")) (LabelS "a") (Br (Lf (LabelS "d")) (LabelS "c") (Lf (LabelS "e")))
+t2 = Br (Br (Lf (LabelS "f")) (LabelS "b") (Lf (LabelS "g"))) (LabelS "a") (Br (Lf (LabelS "d")) (LabelS "c") (Lf (LabelS "e")))
+t3 = Br (Br (Lf (LabelC 'f')) (LabelC 'b') (Lf (LabelC 'g'))) (LabelC 'a') (Br (Lf (LabelC 'd')) (LabelC 'c') (Lf (LabelC 'e')))
+t4 = Br (Lf 2) 1 (Br (Lf 4) 3 (Lf 5))
+t5 = Br (Br (Br (Br (Lf 16) (8) (Lf 17)) (4) (Br (Lf 18) (9) (Lf 19))) (2) (Br (Br (Lf 20) (10) (Lf 21)) (5) (Br (Lf 22) (11) (Lf 23)))) (1) (Br (Br (Br (Lf 24) (12) (Lf 25)) (6) (Br (Lf 26) (13) (Lf 27))) (3) (Br (Br (Lf 28) (14) (Lf 29)) (7) (Br (Lf 30) (15) (Lf 31))))
+t6 = Br (Br (Br (Br (Lf 16) (8) (Lf 17)) (4) (Br (Lf 18) (9) (Lf 19))) (2) (Br (Br (Lf 20) (10) (Lf 21)) (5) (Br (Lf 22) (11) (Lf 23)))) (1) (Lf 3)
+t7 = Br (Br (Br (Br (Lf 16) (8) (Lf 17)) (4) (Br (Lf 18) (9) (Lf 19))) (2) (Br (Br (Lf 20) (10) (Lf 21)) (5) (Br (Lf 22) (11) (Lf 23)))) (1) (Br (Lf 6) 3 (Lf 7) )
+t8 = Br (Br (Br (Br (Lf 16) (8) (Lf 17)) (4) (Lf 9)) (2) (Br (Br (Lf 20) (10) (Lf 21)) (5) (Br (Lf 22) (11) (Lf 23)))) (1) (Br (Lf 6) 3 (Lf 7) )
+
+testsString = [t1, t2]
+testsChar=[t3]
+testsInt=[t4, t5, t6, t7, t8]
+
+stringTrees = map (\(i,x) -> drawSVG ("stringTree"++(show i)++".svg") x) (zip [1..] testsString)
+charTrees = map (\(i,x) -> drawSVG ("charTree"++(show i)++".svg") x) (zip [1..] testsChar)
+intTrees = map (\(i,x) -> drawSVG ("intTree"++(show i)++".svg") x) (zip [1..] testsInt)
+
+main = do 
+	sequence stringTrees
+	sequence charTrees
+	sequence intTrees
